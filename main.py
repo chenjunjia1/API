8e43c53794846fbc5d454461ded778c434b4f0f8
jack Chen <1622758042@qq.com>
2023-09-26 11:22:06 +0800

    New API frame code

---
+++ main.py
@@ -0,0 +1,25 @@
+import unittest
+import allure
+import os
+import subprocess
+from config import BASE_URL
+from api.api_utils import ApiUtils
+from tests.test_api import TestAPIs
+
+# 导入你的测试类
+from tests.test_api import TestAPIs
+
+if __name__ == "__main__":
+    # 运行测试
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.loadTestsFromTestCase(TestAPIs)
+    test_runner = unittest.TextTestRunner(verbosity=2)
+    test_result = test_runner.run(test_suite)
+
+    # 生成Allure报告
+    generate_command = ["allure", "generate", "allure-results", "--clean"]
+    subprocess.run(generate_command, shell=True)
+
+    # 打开Allure报告
+    open_command = ["allure", "open"]
+    subprocess.run(open_command, shell=True)
